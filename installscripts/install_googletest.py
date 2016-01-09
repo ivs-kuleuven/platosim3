6e25aacc523f45b69e9da9feda221c8db1de6eb4
Joris De Ridder <joris@ster.kuleuven.be>
2016-01-09 03:12:28 +0100

    Added the googletest library and a python install script for it.

---
+++ installscripts/install_googletest.py
@@ -0,0 +1,53 @@
+#
+# install.sh runs automatically this script. It can be done manually running (from the same folder than install.sh):
+# $ python ./installscripts/install_hdf5.py
+#
+
+
+import os,shutil,subprocess
+
+
+# Specify the dependency package name
+
+packageName = "googletest"
+
+# Specify build and install folders
+
+currentWorkingDir = os.getcwd()
+buildDir   = currentWorkingDir + "/dependencies/Downloads/"
+installDir = currentWorkingDir + "/dependencies/Installs/" + packageName 
+
+
+# Build and nstall package
+
+installProcedure = "cd {build};                                     \
+					tar -xzvf {package}.tgz;                        \
+					cd {package}/googletest;                        \
+                    mkdir build;                                    \
+                    cd build;                                       \
+                    cmake ..;                                       \
+                    make".format(build=buildDir, package=packageName)
+
+subprocess.call(installProcedure, shell=True)
+
+
+# If there is an older version of the install dir, make sure to remove it first.
+
+shutil.rmtree(installDir, ignore_errors=True)
+
+# Copy the header files.
+
+shutil.copytree(buildDir+packageName+"/googletest/include", installDir+"/include")
+
+# Also copy the libraries
+
+os.mkdir(installDir+"/lib/")
+shutil.copy(buildDir+packageName+"/googletest/build/libgtest.a",      installDir+"/lib/")
+shutil.copy(buildDir+packageName+"/googletest/build/libgtest_main.a", installDir+"/lib/")
+
+# After installation in the install folder, remove the decompressed package folder in 
+# the Downloads dir so that only the .tgz file remains in the Downloads dir.
+
+shutil.rmtree(buildDir+packageName, ignore_errors=True)
+
+
