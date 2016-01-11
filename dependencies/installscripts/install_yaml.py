d1fe82734dab6e6f29745e49a19120abc25f6a34
Joris De Ridder <joris@ster.kuleuven.be>
2016-01-11 17:10:00 +0100

    Added the yaml-cpp dependency, made an install_yaml script, and adapted install.sh accordingly.

---
+++ dependencies/installscripts/install_yaml.py
@@ -0,0 +1,52 @@
+#
+# install.sh runs automatically this script. It can be done manually running (from the same folder than install.sh):
+# $ python ./installscripts/install_yaml.py
+#
+
+
+import os,shutil,subprocess
+
+
+# Specify the dependency package name
+
+packageName = "yaml-cpp"
+
+# Specify build and install folders
+
+currentWorkingDir = os.getcwd()
+buildDir   = currentWorkingDir + "/dependencies/Downloads/"
+installDir = currentWorkingDir + "/dependencies/Installs/" + packageName 
+
+
+# Build and nstall package
+
+installProcedure = "cd {build};                                     \
+					tar -xzvf {package}.tgz;                        \
+					cd {package};                                   \
+                    mkdir build;                                    \
+                    cd build;                                       \
+                    cmake ..;                                       \
+                    make".format(build=buildDir, package=packageName)
+
+subprocess.call(installProcedure, shell=True)
+
+
+# If there is an older version of the install dir, make sure to remove it first.
+
+shutil.rmtree(installDir, ignore_errors=True)
+
+# Copy the header files.
+
+shutil.copytree(buildDir+packageName+"/include", installDir+"/include")
+
+# Also copy the library
+
+os.mkdir(installDir+"/lib/")
+shutil.copy(buildDir+packageName+"/build/libyaml-cpp.a", installDir+"/lib/")
+
+# After installation in the install folder, remove the decompressed package folder in 
+# the Downloads dir so that only the .tgz file remains in the Downloads dir.
+
+shutil.rmtree(buildDir+packageName, ignore_errors=True)
+
+
