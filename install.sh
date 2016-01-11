6940b143879d27d2c795c87c22df69248942b102
Joris De Ridder <joris@ster.kuleuven.be>
2016-01-11 12:21:28 +0100

    Reorganised location of installscripts, adapted install.sh accordingly.

--- install.sh
+++ install.sh
@@ -3,28 +3,20 @@
 # Just run this script:
 # $ ./install.sh
 
-#1. Install dependencies
-#-----------------------
-
-# There is one python script under installscripts/ folder for each dependency package.
-# Each script Unzip/Untar packages in dependencyDownloads/ and
-# installs dependencies packages in dependencyInstalls/
+# Install dependencies
+# There is one python script under dependencies/installscripts/ folder for each 
+# dependency package. Each script Unzip/Untar packages in dependencies/Downloads/ 
+# and installs dependencies packages in dependencies/Installs/
 
 if [ ! -d ./dependencies/Installs ]; then
     mkdir ./dependencies/Installs
 fi
 
-python ./installscripts/install_hdf5.py
-python ./installscripts/install_googletest.py
+python ./dependencies/installscripts/install_hdf5.py
+python ./dependencies/installscripts/install_googletest.py
 
 
-# If any of these packages is already installed, just comment the 
-# corresponding line (please, be sure that the commented package is correctly installed in 
-# your system, otherwise the installation will raise errors!!).
-
-
-#2. Build and install the simulator
-#----------------------------------
+# Build the simulator
 
 if [ ! -d ./build ]; then
     mkdir build
