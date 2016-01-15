e6b71b6e5e5a6d484428274a3409b9ed51c7f597
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-01-14 22:43:30 -0500

    Remove JSon related tests and improved yaml testing.

--- tests/GTestConfigurationParameters.h
+++ tests/GTestConfigurationParameters.h
@@ -1,11 +1,3 @@
-//
-//  GTestInputParameters.h
-//  New PLATO Simulator
-//
-//  Created by Rik Huygen on 26/11/15.
-//  Copyright © 2015 KU Leuven. All rights reserved.
-//
-
 #include <string>
 #include <vector>
 #include <list>
@@ -15,55 +7,67 @@
 #include "ConfigurationParameters.h"
 #include "Exceptions.h"
 
+using namespace std;
 
 TEST(ConfigurationParametersTest, Constructor) {
-
-    ASSERT_THROW(ConfigurationParameters ip = ConfigurationParameters("input.json"), IOException);
 
     ASSERT_THROW(ConfigurationParameters ip = ConfigurationParameters("input.yaml"), IOException);
 
 }
 
-TEST(ConfigurationParametersTest, readGlobalValuesJSon) {
-    ConfigurationParameters ip = ConfigurationParameters("../testData/input.json");
-
-    std::string description = ip.getString("Description");
-    EXPECT_STREQ(description.c_str(), "JSON Input File for 3rd Generation PLATO Simulator");
-
-    std::string author = ip.getString("Author");
-    EXPECT_STREQ(author.c_str(), "Rik Huygen");
-
-    int exposureTime = ip.getInteger("Observing/ExposureTime");
-    EXPECT_EQ(23, exposureTime);
-
-}
-
-TEST(ConfigurationParametersTest, readGlobalValuesYaml) {
+TEST(ConfigurationParametersTest, readGlobalValues) {
     ConfigurationParameters ip = ConfigurationParameters("../testData/input.yaml");
 
-    std::string description = ip.getString("Description");
+    string description = ip.getString("Description");
     EXPECT_STREQ(description.c_str(), "YAML Input File for 3rd Generation PLATO Simulator");
 
-    std::string author = ip.getString("Author");
+    string author = ip.getString("Author");
     EXPECT_STREQ(author.c_str(), "Rik Huygen");
-
-    int exposureTime = ip.getInteger("Observing/ExposureTime");
-    EXPECT_EQ(23, exposureTime);
-
 }
 
 TEST(ConfigurationParametersTest, readGeneralValues) {
-    ConfigurationParameters ip = ConfigurationParameters("../testData/input.json");
+    ConfigurationParameters ip = ConfigurationParameters("../testData/input.yaml");
 
-//    ConfigurationParameters general = ip.getGroup("General");
-
-//    std::string dir2 = general.getString("ProjectLocation");
-//    EXPECT_STREQ(dir1.c_str(), dir2.c_str());
-
-//    std::list<std::string> authors = general.getValues<std::string>("Author");
-//    EXPECT_STREQ(authors.front().c_str(), "Joris De Ridder");
-
-    ASSERT_NO_THROW(ip.getString("Description"));
-//    general.getString("Description");
-//    ASSERT_THROW(general.getString("Description"), IOException);
+    string projectLocation = ip.getString("General/ProjectLocation");
+    EXPECT_STREQ(projectLocation.c_str(), "/Users/rik/Work/PLATO");
 }
+
+TEST(ConfigurationParametersTest, readObservingValues) {
+    ConfigurationParameters ip = ConfigurationParameters("../testData/input.yaml");
+
+    int exposureTime = ip.getInteger("Observing/ExposureTime");
+    EXPECT_EQ(23, exposureTime);
+
+    string filename = ip.getString("Observing/StarCatalogueFileName");
+    EXPECT_STREQ(filename.c_str(), "inputFiles/starField_RA180Dec-70.txt");
+
+    filename = ip.getProjectFileName("Observing/StarCatalogueFileName");
+    EXPECT_STREQ(filename.c_str(), "/Users/rik/Work/PLATO/inputFiles/starField_RA180Dec-70.txt");
+
+    filename = ip.getProjectFileName("Observing/AbsoluteFileName");
+    EXPECT_STREQ(filename.c_str(), "/Users/rik/Work/PLATO/inputFiles/someInputFile.txt");
+
+    double area = ip.getDouble("Observing/LightCollectingArea");
+    EXPECT_DOUBLE_EQ(0.1131, area);
+}
+
+TEST(ConfigurationParametersTest, readSpecialValues) {
+    ConfigurationParameters ip = ConfigurationParameters("../testData/input.yaml");
+
+    int zeroValue = ip.getInteger("Special Values/zero");
+    EXPECT_EQ(0, zeroValue);
+
+    int oneValue = ip.getInteger("Special Values/one");
+    EXPECT_EQ(1, oneValue);
+
+    int minusOneValue = ip.getInteger("Special Values/minus-one");
+    EXPECT_EQ(-1, minusOneValue);
+
+    bool booleanTrue = ip.getBoolean("Special Values/boolean-true");
+    EXPECT_TRUE(booleanTrue);
+
+    bool booleanFalse = ip.getBoolean("Special Values/boolean-false");
+    EXPECT_FALSE(booleanFalse);
+
+}
+
