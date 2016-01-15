8a41c49ca63ab1f93cebf7e4315cb45bf444bd64
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-01-14 22:41:48 -0500

    Use more portable code to check file properties.

--- include/FileUtilities.h
+++ include/FileUtilities.h
@@ -1,27 +1,18 @@
-//
-//  FileUtilities.h
-//  New PLATO Simulator
-//
-//  Created by Rik Huygen on 27/11/15.
-//  Copyright © 2015 KU Leuven. All rights reserved.
-//
-
 #ifndef FILE_UTILITIES_H
 #define FILE_UTILITIES_H
 
 #include <string>
-#include <sys/stat.h>
+#include <fstream>
 
-/**
- * PURPOSE: Check if a file exists
- * 
- * INPUTS:  filename
- * 
- * OUTPUTS: return true if and only if the file exists, false otherwise
- */
-static bool fileExists(const std::string& file) {
-    struct stat buf;
-    return (stat(file.c_str(), &buf) == 0);
-}
+using namespace std;
+
+class FileUtilities
+{
+public:
+    static bool fileExists(const string &filename);
+    static bool isRelative(const string &filename);
+};
+
+
 
 #endif /* FileUtilities_hpp */
--- source/FileUtilities.cpp
+++ source/FileUtilities.cpp
@@ -1,9 +1,28 @@
-//
-//  FileUtilities.cpp
-//  New PLATO Simulator
-//
-//  Created by Rik Huygen on 27/11/15.
-//  Copyright © 2015 KU Leuven. All rights reserved.
-//
-
 #include "FileUtilities.h"
+
+/**
+ * PURPOSE: Check if a file exists
+ * 
+ * INPUTS:  filename
+ * 
+ * OUTPUTS: return true if and only if the file exists, false otherwise
+ */
+bool FileUtilities::fileExists(const string &filename) {
+    ifstream fin(filename);
+    return fin.good();
+}
+
+
+
+
+
+bool FileUtilities::isRelative(const string &filename) {
+    if (filename[0] == '/')
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
