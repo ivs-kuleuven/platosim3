bc75d9013592dc688bd9e5841f78a30fc3045081
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-01-16 00:15:35 -0500

    Remove obsolete comments at the top

--- tests/AllTests.cpp
+++ tests/AllTests.cpp
@@ -1,11 +1,3 @@
-//
-//  AllTests.cpp
-//  New PLATO Simulator
-//
-//  Created by Rik Huygen on 27/11/15.
-//  Copyright © 2015 KU Leuven. All rights reserved.
-//
-
 
 // Include all the tests here
 
