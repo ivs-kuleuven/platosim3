1e1b9371755898bc15e28221e3812bb036ca424c
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-01-16 00:16:06 -0500

    Add simple tests for isRelative.

--- tests/GTestFileUtilities.h
+++ tests/GTestFileUtilities.h
@@ -9,3 +9,13 @@ TEST(FileUtilitiesTest, fileExists) {
     ASSERT_FALSE(FileUtilities::fileExists("../wrongDirectory/input.yaml"));
 
 }
+
+
+TEST(FileUtilitiesTest, isRelative) {
+    
+    ASSERT_TRUE(FileUtilities::isRelative("../testData/input.yaml"));
+
+    ASSERT_FALSE(FileUtilities::isRelative("/Users/rik/Git/PlatoSim3/testData/input.yaml"));
+
+}
+
