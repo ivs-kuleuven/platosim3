3ca6d79317aaa25c43fe8d470c87a1383ae909b0
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-01-15 20:49:28 -0500

    Add vertical spacing and add tests for conversions.

--- tests/GTestConfigurationParameters.h
+++ tests/GTestConfigurationParameters.h
@@ -9,13 +9,21 @@
 
 using namespace std;
 
-TEST(ConfigurationParametersTest, Constructor) {
+
+
+TEST(ConfigurationParametersTest, Constructor)
+{
 
     ASSERT_THROW(ConfigurationParameters ip = ConfigurationParameters("input.yaml"), IOException);
 
 }
 
-TEST(ConfigurationParametersTest, readGlobalValues) {
+
+
+
+
+TEST(ConfigurationParametersTest, readGlobalValues)
+{
     ConfigurationParameters ip = ConfigurationParameters("../testData/input.yaml");
 
     string description = ip.getString("Description");
@@ -25,14 +33,24 @@ TEST(ConfigurationParametersTest, readGlobalValues) {
     EXPECT_STREQ(author.c_str(), "Rik Huygen");
 }
 
-TEST(ConfigurationParametersTest, readGeneralValues) {
+
+
+
+
+TEST(ConfigurationParametersTest, readGeneralValues)
+{
     ConfigurationParameters ip = ConfigurationParameters("../testData/input.yaml");
 
     string projectLocation = ip.getString("General/ProjectLocation");
     EXPECT_STREQ(projectLocation.c_str(), "/Users/rik/Work/PLATO");
 }
 
-TEST(ConfigurationParametersTest, readObservingValues) {
+
+
+
+
+TEST(ConfigurationParametersTest, readObservingValues)
+{
     ConfigurationParameters ip = ConfigurationParameters("../testData/input.yaml");
 
     int exposureTime = ip.getInteger("Observing/ExposureTime");
@@ -41,17 +59,22 @@ TEST(ConfigurationParametersTest, readObservingValues) {
     string filename = ip.getString("Observing/StarCatalogueFileName");
     EXPECT_STREQ(filename.c_str(), "inputFiles/starField_RA180Dec-70.txt");
 
-    filename = ip.getProjectFileName("Observing/StarCatalogueFileName");
+    filename = ip.getAbsoluteFileName("Observing/StarCatalogueFileName");
     EXPECT_STREQ(filename.c_str(), "/Users/rik/Work/PLATO/inputFiles/starField_RA180Dec-70.txt");
 
-    filename = ip.getProjectFileName("Observing/AbsoluteFileName");
+    filename = ip.getAbsoluteFileName("Observing/AbsoluteFileName");
     EXPECT_STREQ(filename.c_str(), "/Users/rik/Work/PLATO/inputFiles/someInputFile.txt");
 
     double area = ip.getDouble("Observing/LightCollectingArea");
     EXPECT_DOUBLE_EQ(0.1131, area);
 }
 
-TEST(ConfigurationParametersTest, readSpecialValues) {
+
+
+
+
+TEST(ConfigurationParametersTest, readSpecialValues)
+{
     ConfigurationParameters ip = ConfigurationParameters("../testData/input.yaml");
 
     int zeroValue = ip.getInteger("Special Values/zero");
@@ -63,6 +86,10 @@ TEST(ConfigurationParametersTest, readSpecialValues) {
     int minusOneValue = ip.getInteger("Special Values/minus-one");
     EXPECT_EQ(-1, minusOneValue);
 
+    // A 0 or a 1 can not be converted into a Boolean - CHECK THIS!
+    ASSERT_ANY_THROW(ip.getBoolean("Special Values/zero"));
+    ASSERT_ANY_THROW(ip.getBoolean("Special Values/one"));
+
     bool booleanTrue = ip.getBoolean("Special Values/boolean-true");
     EXPECT_TRUE(booleanTrue);
 
@@ -70,4 +97,29 @@ TEST(ConfigurationParametersTest, readSpecialValues) {
     EXPECT_FALSE(booleanFalse);
 
 }
+
+
+
+
+
+TEST(ConfigurationParametersTest, testConversions)
+{
+    ConfigurationParameters ip = ConfigurationParameters("../testData/input.yaml");
+
+    // Can convert an integer value into a double
+    double exposureTime = ip.getDouble("Observing/ExposureTime");
+    EXPECT_DOUBLE_EQ(23.0, exposureTime);
+
+    // Can convert an integer value into a string
+    string exposureTimeString = ip.getString("Observing/ExposureTime");
+    EXPECT_STREQ("23", exposureTimeString.c_str());
+
+    // Can not convert a double value into an Integer
+    ASSERT_ANY_THROW(ip.getInteger("Observing/LightCollectingArea"));
+
+    // Conversion from Integer 23 to Boolean should also throw an exception
+    ASSERT_ANY_THROW(ip.getBoolean("Observing/ExposureTime"));
+
+}
+
 
