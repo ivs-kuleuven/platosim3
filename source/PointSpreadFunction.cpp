b3fe66580fd8883213c76d1c0803777c9ad456ed
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-01-21 19:00:23 +0100

    Skeleton for the PSF class

---
+++ source/PointSpreadFunction.cpp
@@ -0,0 +1,45 @@
+#include "PointSpreadFunction.h"
+
+
+PointSpreadFunction::PointSpreadFunction(ConfigurationParameters configurationParameters)
+{
+
+}
+
+
+
+
+
+
+PointSpreadFunction::~PointSpreadFunction()
+{
+
+}
+
+
+
+
+
+void PointSpreadFunction::select()
+{
+
+}
+
+
+
+
+
+void PointSpreadFunction::rotate()
+{
+
+}
+
+
+
+
+
+void PointSpreadFunction::rebin()
+{
+
+}
+
