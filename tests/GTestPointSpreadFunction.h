93795ab396435ddd296a3f1f020d8844283e2255
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-01-25 09:22:23 +0100

    Start unit test for PointSpreadFunction

---
+++ tests/GTestPointSpreadFunction.h
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+#include "PointSpreadFunction.h"
+
+using namespace std;
+
+TEST(PointSpreadFunctionTest, Constructor_ConfigurationParameters)
+{
+    ConfigurationParameters cp = ConfigurationParameters("../testData/input.yaml");
+    PointSpreadFunction psf = PointSpreadFunction(cp);
+
+}
