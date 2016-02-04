cee5f86c2ea42e926f438060b870b412960dcfcf
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-02-04 11:28:42 +0100

    Add some pretty printers for exceptions

---
+++ tests/PrettyPrinters.h
@@ -0,0 +1,35 @@
+#ifndef PRETTY_PRINTERS_H
+#define PRETTY_PRINTERS_H
+
+#include <iostream>
+#include <string>
+
+#include "Logger.h"
+#include "Exceptions.h"
+
+// It's important that PrintTo() for each class is defined in the SAME
+// namespace that defines the class.  C++'s look-up rules rely on that.
+
+
+
+
+void PrintTo(const FileException& fe, ::std::ostream* os) {
+    *os << fe.what();
+}
+
+void PrintTo(const IOException& ioe, ::std::ostream* os) {
+    *os << ioe.what();
+}
+
+void PrintTo(const UnsupportedException& ue, ::std::ostream* os) {
+    *os << ue.what();
+}
+
+void PrintTo(const IllegalArgumentException& iae, ::std::ostream* os) {
+    *os << iae.what();
+}
+
+
+
+
+#endif // PRETTY_PRINTERS_H
