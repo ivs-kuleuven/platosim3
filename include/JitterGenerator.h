bd44afc087d8b3ac8a63fd78b531c26373bf2c1f
Joris De Ridder <joris@ster.kuleuven.be>
2016-02-10 14:11:58 +0100

    Made JitterGenerator an abstract base clase.

--- include/JitterGenerator.h
+++ include/JitterGenerator.h
@@ -1,10 +1,12 @@
-#ifndef JITTER_H
-#define JITTER_H
+#ifndef JITTERGENERATOR_H
+#define JITTERGENERATOR_H
 
 #include <string>
 #include <vector>
 
 #include "Logger.h"
+#include "Heartbeat.h"
+#include "HDF5Writer.h"
 #include "ConfigurationParameters.h"
 
 
@@ -12,14 +14,15 @@ using namespace std;
 
 
 
-class JitterGenerator
+class JitterGenerator : public Heartbeat
 {
     public:
 
         JitterGenerator(ConfigurationParameters &configurationParameters);
         ~JitterGenerator();
 
-        virtual void getNextYawPitchRoll(double &yaw, double &pitch, double &roll, double timeInterval);
+        virtual void configure(ConfigurationParameters &configParams);
+        virtual void getNextYawPitchRoll(double &yaw, double &pitch, double &roll, double timeInterval) = 0;
 
     protected:
 
