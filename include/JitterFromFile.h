f07c3732c311a20ccc0fa4f92f275fa5392485b0
Joris De Ridder <joris@ster.kuleuven.be>
2016-02-10 16:31:30 +0100

    First version of JitterFromFile class.

---
+++ include/JitterFromFile.h
@@ -0,0 +1,51 @@
+#ifndef JITTERFROMFILE_H
+#define JITTERFROMFILE_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+
+#include "Logger.h"
+#include "Heartbeat.h"
+#include "HDF5Writer.h"
+#include "ConfigurationParameters.h"
+#include "JitterGenerator.h"
+
+
+using namespace std;
+
+
+
+class JitterFromFile : public JitterGenerator
+{
+    public:
+
+        JitterFromFile(ConfigurationParameters &configurationParameters);
+        ~JitterFromFile();
+
+        virtual void configure(ConfigurationParameters &configParams);
+        virtual void getNextYawPitchRoll(double &yaw, double &pitch, double &roll, double timeInterval) override;
+        virtual double getHeartbeatInterval() override;
+
+    protected:
+
+        string pathToJitterFile;
+
+        int timeIndex;
+
+        vector<double> time;
+        vector<double> yaw;
+        vector<double> pitch;
+        vector<double> roll;
+
+
+    private:
+
+ };
+
+
+
+#endif
+ 
