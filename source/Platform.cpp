d9341e2f8f1d08f6350dcd219466a6a0056b4049
Joris De Ridder <joris@ster.kuleuven.be>
2016-02-11 18:48:58 +0100

    First version of Platform::rotateYawPitchRoll().

--- source/Platform.cpp
+++ source/Platform.cpp
@@ -138,3 +138,66 @@ double Platform::getHeartbeatInterval()
 
 
 
+
+
+
+
+
+/**
+ * \brief Transforms the coordinates of a vector after yaw, pitch, and roll rotations.
+ * 
+ * \details We define the yaw, the pitch, and the roll as rotation angles around respectively 
+ *          the X_SC, Y_SC, and Z_SC axes, (SC standing for spacecraft reference frame) such that 
+ *          the angles increase with a clockwise rotation, when looking along the positive axes. 
+ *          First a roll rotation is done around the Z_SC axis, then a pitch rotation is done 
+ *          around the rotated Y_SC axis, and finally a yaw rotation is done around the already
+ *          twice-rotated X_SC axis. The combined rotation matrix is: 
+ *                 R(yaw, pitch, roll) = R(yaw) * R(pitch) * R(roll)
+ *          Which transforms
+ *                 vector_before = R(yaw, pitch, roll) * vector_after
+ *          where vector_before and vector_after are the coordinates of the same point before and
+ *          after the yaw.pitch,roll rotations. The inverse transformation (usd in this function) is
+ *                 R^{-1}(yaw, pitch, roll) = R^t(roll) * R^t(pitch) * R^t(yaw)
+ *          which transforms
+ *                 vector_after = R(yaw, pitch, roll) * vector_before
+ *          Here ^t denotes the transpose. 
+ * 
+ * \param coord Coordinates of the point in the spacecraft reference frame before the rotation 
+ * \param yaw   Yaw angle   [rad]
+ * \param pitch Pitch angle [rad]
+ * \param roll  Roll angle  [rad]
+ * \return      Coordinates of the point in the spacecraft reference frame after the rotations
+ */
+
+arma::colvec Platform::rotateYawPitchRoll(arma::colvec coord, const double yaw, const double pitch, const double roll)
+{
+    // Some handy abbreviations
+
+    const double sinYaw   = sin(yaw);
+    const double cosYaw   = cos(yaw);
+    const double sinPitch = sin(pitch);
+    const double cosPitch = cos(pitch);
+    const double sinRoll  = sin(roll);
+    const double cosRoll  = cos(roll);
+
+    // The rotation matrices
+
+    arma::mat Ryaw = {{1.0,    0.0,     0.0},
+                      {0.0, cosYaw, -sinYaw},
+                      {0.0, sinYaw,  cosYaw}};
+
+    arma::mat Rpitch = {{ cosPitch, 0.0, sinPitch},
+                        {      0.0, 1.0,      0.0},
+                        {-sinPitch, 0.0, cosPitch}};
+
+    arma::mat Rroll = {{cosRoll, -sinRoll, 0.0},
+                       {sinRoll,  cosRoll, 0.0},
+                       {    0.0,      0.0, 1.0}}; 
+
+    // Do the transformation
+
+    return Rroll.t() * Rpitch.t() * Ryaw.t() * coord;
+}
+
+
+
