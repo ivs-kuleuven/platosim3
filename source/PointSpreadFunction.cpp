8e7c0633204a8cc72223fb8433ce8b3f3dd75f3b
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-02-19 08:07:37 +0100

    move rotate and select methods into public. Add status variable to keep track if the PSF has been rotated or not.Add lookup table for radius at which PSF is generated.First version of select and rotate methods.

--- source/PointSpreadFunction.cpp
+++ source/PointSpreadFunction.cpp
@@ -2,7 +2,7 @@
 #include "Units.h"
 #include "Exceptions.h"
 #include "Logger.h"
-
+#include "ArrayOperations.h"
 
 PointSpreadFunction::PointSpreadFunction(ConfigurationParameters &configParam)
 {
@@ -11,6 +11,7 @@ PointSpreadFunction::PointSpreadFunction(ConfigurationParameters &configParam)
     groupName = "6000";  // this is currently the only group defined in the HDF5 file
 
     isSelected = false;
+    isRotated = false;
     
     hdf5file = new HDF5File(location);
 
@@ -49,10 +50,38 @@ void PointSpreadFunction::configure(ConfigurationParameters &cp)
 
 
 
-
-void PointSpreadFunction::select()
+void PointSpreadFunction::select(double radius)
 {
+    if (isSelected)
+    {
+        Log.warning("Another PSF was previously selected.");
+    }
 
+    // Convert radius into the string id that identifies the psf dataset in the HDF5 file
+    // We work with a lookup table psfdata::radius which contains fixed radius values for which PSF data
+    // was generated. 
+    
+    arma::vec rads = psfdata::radius - radius;
+    rads = abs(rads);
+
+    arma::uword index;
+    double radius_lut = rads.min(index);
+
+    if (index > psfdata::radius.n_elem-1)
+    {
+        Log.warning("Radius index out of bounds.");
+        index = psfdata::radius.n_elem-1;
+    }
+
+    string id = "ar" + to_string(int(psfdata::radius(index) * 1000));
+
+    Log.debug("Identifier for selected PSF is " + id);
+
+    // Load the psf array into the psfMap
+    
+    hdf5file->readArray("/" + groupName, "ar00000", psfMap);
+
+    isSelected = true;
 }
 
 
@@ -63,6 +92,16 @@ void PointSpreadFunction::select()
 
 void PointSpreadFunction::rotate(double angle)
 {
+    if (isRotated)
+    {
+        Log.warning("The PSF has been previously rotated and will not be rotated again because of inaccuracies.");
+        Log.warning("TODO: reload the PSF from the inputfile before rotating.");
+    }
+    else
+    {
+        psfMap = rotateArray(psfMap, angle);
+        isRotated = true;        
+    }
 }
 
 
