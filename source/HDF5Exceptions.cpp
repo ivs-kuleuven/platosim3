e6b94be00d2b6effa54d1bfb5c5743b01ef57dc4
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-02-23 09:26:49 +0100

    layout changes

--- source/HDF5Exceptions.cpp
+++ source/HDF5Exceptions.cpp
@@ -9,11 +9,13 @@ using namespace std;
 
 
 
-const char * H5FileException::what() const throw() {
+const char * H5FileException::what() const throw() 
+{
     return message.c_str();
 }
 
-H5FileException::H5FileException(string msg) {
+H5FileException::H5FileException(string msg) 
+{
     message = msg;
     Log.error(message);
 }
@@ -27,11 +29,13 @@ H5FileException::~H5FileException() throw() {}
 
 
 
-const char * H5GroupException::what() const throw() {
+const char * H5GroupException::what() const throw() 
+{
     return message.c_str();
 }
 
-H5GroupException::H5GroupException(string msg) {
+H5GroupException::H5GroupException(string msg) 
+{
     message = msg;
     Log.error(message);
 }
@@ -45,11 +49,13 @@ H5GroupException::~H5GroupException() throw() {}
 
 
 
-const char * H5DatasetException::what() const throw() {
+const char * H5DatasetException::what() const throw() 
+{
     return message.c_str();
 }
 
-H5DatasetException::H5DatasetException(string msg) {
+H5DatasetException::H5DatasetException(string msg) 
+{
     message = msg;
     Log.error(message);
 }
@@ -63,14 +69,18 @@ H5DatasetException::~H5DatasetException() throw() {}
 
 
 
-const char * H5AttributeException::what() const throw() {
+const char * H5AttributeException::what() const throw() 
+{
     return message.c_str();
 }
 
-H5AttributeException::H5AttributeException(string msg) {
+H5AttributeException::H5AttributeException(string msg) 
+{
     message = msg;
     Log.error(message);
 }
 
 H5AttributeException::~H5AttributeException() throw() {}
+
+
 
