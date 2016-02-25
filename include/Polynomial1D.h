5aec587b76b6a2d22139a22423c596d735b7b82d
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-02-25 20:22:27 +0100

    First version of 1D polynomial similar to 2D polynomial

---
+++ include/Polynomial1D.h
@@ -0,0 +1,27 @@
+#ifndef POLYNOMIAL_1D_H
+#define POLYNOMIAL_1D_H
+
+
+#include <iostream>
+
+#include "Exceptions.h"
+#include "Logger.h"
+#include "StringUtilities.h"
+
+class Polynomial1D 
+{
+
+    public:
+        Polynomial1D(int degree, double coefficients[]);
+        ~Polynomial1D();
+
+        double getCoefficient(int index);
+        double evaluateAt(double x);    
+
+    private:
+        int degree;
+        double *coefficients;
+};
+
+
+#endif /* POLYNOMIAL_1D_H */
---
+++ source/Polynomial1D.cpp
@@ -0,0 +1,85 @@
+#include "Polynomial1D.h"
+
+using namespace std;
+
+
+
+
+
+
+/**
+ * \brief      Constructor for the Polynomial1D class
+ * 
+ * \detail
+ * 
+ * The general 1D polynomial of degree n is
+ * 
+ * \f[P(x) = c_{0} + c_{1} x + c_{2} x^{2} + ... + c_{n} x^{n}\f]
+ *
+ * \param[in]  deg    the degree of the Polynomial
+ * \param      coeff  the coeficients of the polynomial
+ * 
+ */
+Polynomial1D::Polynomial1D(int deg, double coeff[])
+{
+    degree = deg;
+    unsigned int n_terms = degree + 1;
+
+    Log.debug("Number of terms for " + to_string(degree) + " is " + to_string(n_terms));
+
+    coefficients = new double[n_terms];
+
+    for( unsigned int i = 0; i < n_terms; i++)
+    {
+        coefficients[i] = coeff[i];
+    }
+}
+
+
+
+
+
+
+
+/**
+ * \brief      Destructor, free coefficients memory
+ */
+Polynomial1D::~Polynomial1D()
+{
+    delete [] coefficients;
+}
+
+
+
+
+
+
+
+
+/**
+ * \brief      Evaluate the 1D polynomial in x
+ *
+ * \param[in]  x     x-value
+ *
+ * \return     y the evaluated polynomial
+ */
+double Polynomial1D::evaluateAt(double x)
+{
+
+    using StringUtilities::dtos;
+
+    double sum = coefficients[0];
+    double xPow = 1.0;
+
+    Log.debug("sum = " + dtos(sum));
+
+    for(unsigned int i = 1; i <= degree; i++)
+    {
+        xPow *= x;
+        sum += xPow * coefficients[i];
+        Log.debug("sum = " + dtos(sum));
+    }
+
+    return sum;
+}
+
