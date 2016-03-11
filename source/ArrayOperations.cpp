6b737059657bb416b5137f2d6bdae5e2f3d396c3
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-03-11 11:45:35 +0100

    Add first version of rebining functions.

--- source/ArrayOperations.cpp
+++ source/ArrayOperations.cpp
@@ -271,3 +271,168 @@ arma::fmat rotateArrayBilinear(arma::fmat origArray, double angle)
 
 
 
+
+
+
+/**
+ * @brief      Method that rebins an array to the given new resolution.
+ *
+ * @details    
+ * 
+ * The array is considered square and the new dimension must be a multiple or 
+ * a factor of the dimension of array.
+ * 
+ * 
+ * 
+ * Remember the arrays are column-major.
+ *
+ * @param[in]  array             the original array that needs rebinning
+ * @param[in]  sourceResolution  the number of elements that make up a group in the source
+ * @param[in]  targetResolution  the number of elements that make up a group in the target
+ * @param[in]  xCenter           the center x point of the original array (zero-based)
+ * @param[in]  yCenter           the center y point of the original array (zero-based)
+ *
+ * @return     The new rebinned array
+ */
+arma::fmat rebin(arma::fmat array, unsigned int sourceResolution, unsigned int targetResolution,
+                       unsigned int xCenter, unsigned int yCenter)
+{
+    // Create an intermediate array of odd size for which the centre point of the
+    // original array lies in the middle of the intermediate array.
+    // The new intermediate array shall be big enough so the complete original array fits in.
+
+    unsigned int  sizeIntermediateArray = max(
+        (unsigned int) max(xCenter + 1, yCenter + 1),
+        (unsigned int) max(array.n_rows - xCenter, array.n_cols - yCenter)) * 2 - 1;
+
+    // The dimension of the intermediate array shall be a multiple of the source resolution.
+
+    sizeIntermediateArray = sizeIntermediateArray + (sizeIntermediateArray % sourceResolution) * 2;
+
+    arma::fmat intermediateArray(sizeIntermediateArray, sizeIntermediateArray, arma::fill::zeros);
+
+    // Copy the original array to the intermediate array and make sure the center point for the original
+    // array is now the real center of the intermediate array.
+
+    double centerIntermediateArray = (sizeIntermediateArray - 1) / 2.0;
+
+    intermediateArray(
+        arma::span(centerIntermediateArray - xCenter, centerIntermediateArray - xCenter + array.n_rows - 1),
+        arma::span(centerIntermediateArray - yCenter, centerIntermediateArray - yCenter + array.n_cols - 1)) =
+        array;
+
+
+    double binning = double(sourceResolution) / double(targetResolution);
+
+    // Size of the rebinned, target array
+
+    unsigned int sizeTargetArray = int(sizeIntermediateArray / binning);
+
+    arma::fmat target;
+
+    if (binning >= 1.0)
+    {
+
+        if (sizeTargetArray % 2 == 0)
+            sizeTargetArray++;
+
+        target = arma::zeros<arma::fmat>(sizeTargetArray, sizeTargetArray);
+
+        for (int row = 0; row < sizeTargetArray; row++) 
+        {
+            for (int column = 0; column < sizeTargetArray; column++) 
+            {
+                if ((row + 1) * binning - 1 < sizeIntermediateArray
+                        && (column + 1) * binning - 1 < sizeIntermediateArray)
+                {
+                    target(row, column) = arma::accu(
+                        intermediateArray(arma::span(row * binning, (row + 1) * binning - 1),
+                                          arma::span(column * binning, (column + 1) * binning - 1)));
+                }
+            }
+        }
+    }
+    else
+    {
+        stringstream fullMessage;
+
+        fullMessage << "ArrayOperations.rebin: Rebinning is only supported to smaller dimensions, i.e. targetResolution (" 
+                    << to_string(targetResolution) << "), ";
+        fullMessage << "must be smaller than sourceResolution (" << to_string(sourceResolution) << ")." << endl;
+
+        throw UnsupportedException(fullMessage.str());
+    }
+
+    return target;
+}
+
+
+
+
+
+
+
+
+
+/**
+ * @brief      Resize an array to the new dimensions.
+ *
+ * @details
+ * 
+ * There are currently some restrictions on the rebinning:
+ * 
+ * \li only square arrays can be rebinned
+ * \li rebinning works only towards smaller dimensions
+ * \li the dimension of the rebinned array must be an integer fraction of the original array
+ * 
+ * @param[in]  array       the array that needs to be rebinned
+ * @param[in]  n_rows_new  the number of rows in the rebinned array
+ * @param[in]  n_cols_new  the number of columns in the new array
+ *
+ * @return     the rebinned array
+ */
+arma::fmat rebin(arma::fmat array, unsigned int n_rows_new, unsigned int n_cols_new)
+{
+    arma::fmat rebinnedArray = arma::zeros<arma::fmat>(n_rows_new, n_cols_new);
+
+    if (n_rows_new != n_cols_new)
+    {
+        throw IllegalArgumentException(string("ArrayOperations.rebin: ") + 
+            "rebinning currently only implemented for a square array, n_rows_new should equal n_cols_new.");
+    }
+
+    if (n_rows_new > array.n_rows)
+    {
+        throw IllegalArgumentException(string("ArrayOperations.rebin: ") + 
+            "rebinning currently only implemented to smaller dimensions.");
+    }
+
+    if (array.n_rows % n_rows_new)
+    {
+        throw IllegalArgumentException(string("ArrayOperations.rebin: ") + 
+            "new dimensions must be a integer fraction of original dimensions.");
+
+    }
+
+    unsigned int binning = array.n_rows / n_rows_new;
+
+    // Rebinning is simply done by adding all values of the sub-pixels per pixel.
+
+    for (unsigned int row = 0; row < n_rows_new; row++)
+    {
+        for (unsigned int column = 0; column < n_cols_new; column++)
+        {
+            const unsigned int beginRow = row * binning;
+            const unsigned int beginCol = column * binning;
+            const unsigned int endRow = (row + 1) * binning - 1;
+            const unsigned int endCol = (column + 1) * binning - 1;
+
+            rebinnedArray(row, column) = arma::accu(array.submat(beginRow, beginCol, endRow, endCol));
+        }
+    }
+
+    return rebinnedArray;
+}
+
+
+
