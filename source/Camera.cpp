0402d0fddbb1dca4ebfcfca4997306963ebe6b2e
Rik Huygen <rik.huygen@ster.kuleuven.be>
2016-03-30 18:39:32 +0200

    include field distortion also when calculating coordinates for the subfield in Camera.ccp
    Former-commit-id: 914b2aafa9ab311249da29091bc4e70cf657c8e2

--- source/Camera.cpp
+++ source/Camera.cpp
@@ -291,7 +291,13 @@ void Camera::exposeDetector(Detector &detector, double startTime, double exposur
     Log.debug("Camera: lower left corner of subfield at (Xmm, Ymm) = (" + to_string(corner00Xmm) + ", " + to_string(corner00Ymm) + ") mm");
     Log.debug("Camera: upper right corner of subfield at (Xmm, Ymm) = (" + to_string(corner11Xmm) + ", " + to_string(corner11Ymm) + ") mm");
 
-
+    if (includeFieldDistortion)
+    {
+        // Convert the planar [mm] to distorted [mm] focal plane coordinates
+        tie(centerXmm, centerYmm) = planarToDistortedFocalPlaneCoordinates(centerXmm, centerYmm);
+        tie(corner00Xmm, corner00Ymm) = planarToDistortedFocalPlaneCoordinates(corner00Xmm, corner00Ymm);
+        tie(corner11Xmm, corner11Ymm) = planarToDistortedFocalPlaneCoordinates(corner11Xmm, corner11Ymm);
+    }
 
     // Convert the planar [mm] to angular [rad] focal plane coordinates 
 
