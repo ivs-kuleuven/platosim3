51b72e7f464281ee130d0ff58d099e56ee98b06f
Sara Regibo <sara@ster.kuleuven.be>
2016-09-22 12:20:49 +0200

    Correction of typing mistake.
    Correction of typing mistake in the documentation of the applyShort2013CTIModel() method.

--- source/Detector.cpp
+++ source/Detector.cpp
@@ -1259,7 +1259,7 @@ void Detector::applySimpleCTImodel()
 /**
  * \brief: Apply the effect of the charge-transfer inefficiency to the pixel map,
  *         using the model described in Short et al., MNRAS 430, 3078-3085 (2013).
- *         Only paralllell readout is taken into account here.
+ *         Only parallel readout is taken into account here.
  *         
  * \note The readout register is assumed to be right next to row [0] of the pixel map.
  *       The pixel map needs to be in [e-], not in [ADU]
