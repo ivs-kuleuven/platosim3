73290a96b7ac473a82cbb29759ffdee6792c4b23
Sara Regibo <sara@ster.kuleuven.be>
2017-02-13 11:25:17 +0100

    Class representing the front-end electronics (FEE).
    This class is required to take into account:
    
    * FEE readout noise
    * (temperature dependent) FEE gain
    
    and other effects, still to be implemented.

---
+++ include/FrontEndElectronics.h
@@ -0,0 +1,60 @@
+#ifndef FRONTENDELECTRONICS_H
+#define FRONTENDELECTRONICS_H
+
+#include <string>
+#include <cmath>
+#include <random>
+#include <functional>
+
+#include "armadillo"
+
+#include "Constants.h"
+#include "ArrayOperations.h"
+#include "ConfigurationParameters.h"
+#include "HDF5File.h"
+#include "HDF5Writer.h"
+#include "Logger.h"
+#include "Units.h"
+
+using namespace std;
+
+class FrontEndElectronics: public HDF5Writer {
+public:
+
+	FrontEndElectronics(ConfigurationParameters &configParam, HDF5File &hdf5File);
+	virtual ~FrontEndElectronics();
+
+	virtual void configure(ConfigurationParameters &configParam);
+
+	virtual double getNominalOperatingTemperature(){return nominalOperatingTemperature;};		// Nominal operating temperature of the FEE [K]
+	virtual double getTemperature();						// Current FEE temperature
+
+	virtual double getReadoutNoise(){return readoutNoise;};					// FEE readout noise [e-/pixel]
+
+	virtual double getGainRefValueLeft(){return refValueGainLeft;};
+	virtual double getGainRefValueRight(){return refValueGainRight;};
+	virtual double getGainStability(){return gainStability;};
+
+protected:
+
+	virtual void generateGain();
+
+
+	double nominalOperatingTemperature;					// Nominal operating temperature of the FEE [K]
+
+	double readoutNoise;									// FEE readout noise [e-/pixel]
+
+	double refValueGain;									// Reference value for the FEE gain [µV/e-]
+	double gainStability;								// Gain stability for the FEE [µV/e-/K]
+	double gainDelta;									// Allowed difference in gain between the left and the right half of the detector [%]
+
+	double refValueGainLeft;								// Reference value for the gain on the ACD reading the left-hand side of the detector [µV/e-]
+	double refValueGainRight;							// Reference value for the gain on the ACD reading the right-hand side of the detector [µV/e-]
+
+	long gainSeed;
+
+private:
+
+};
+
+#endif
---
+++ source/FrontEndElectronics.cpp
@@ -0,0 +1,93 @@
+#include "FrontEndElectronics.h"
+
+FrontEndElectronics::FrontEndElectronics(ConfigurationParameters &configParam, HDF5File &hdf5file)
+: HDF5Writer(hdf5file)
+{
+	// Parse the parameters from the configuration file.
+
+	configure(configParam);
+
+	// Generate FEE gain
+
+	generateGain();
+}
+
+
+
+
+
+
+
+
+/**
+ * Destructor.
+ *
+ */
+FrontEndElectronics::~FrontEndElectronics()
+{
+	flushOutput();
+}
+
+
+
+
+
+
+
+
+
+
+void FrontEndElectronics::configure(ConfigurationParameters &configParam)
+ {
+	nominalOperatingTemperature = configParam.getDouble("FEE/NominalOperatingTemp");
+
+	readoutNoise = configParam.getDouble("FEE/ReadoutNoise");
+
+	refValueGain  = configParam.getDouble("FEE/Gain/RefValue");
+	gainStability = configParam.getDouble("FEE/Gain/Stability");
+	gainDelta     = configParam.getDouble("FEE/Gain/Delta");
+	gainSeed      = configParam.getLong("RandomSeeds/FeeGainSeed");
+ }
+
+
+
+
+
+
+
+
+
+
+/**
+ * Generate FEE gain for the left and the right ADC.
+ */
+void FrontEndElectronics::generateGain()
+{
+	mt19937 gainGenerator;
+	gainGenerator.seed(gainSeed);
+
+	normal_distribution<double> gainDistribution = normal_distribution<double>(
+			refValueGain, gainDelta * refValueGain);
+
+	refValueGainLeft = gainDistribution(gainGenerator);
+	refValueGainRight = gainDistribution(gainGenerator);
+}
+
+
+
+
+
+
+
+
+
+
+/**
+ * \brief Returns the current temperature of the FEE, expressed in K.
+ *
+ * \note In the current implementation, we assume the temperature to be constant and equal to
+ *       the nominal operating temperature of the FEE.
+ */
+double FrontEndElectronics::getTemperature(){
+	return nominalOperatingTemperature;
+}
