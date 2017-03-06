24e1cbc5483a22a5d43a5df16938cb875e784625
Joris De Ridder <joris@ster.kuleuven.be>
2017-03-06 11:36:08 +0100

    First embryonic not-working version of the DetectorWithAnalyticPSF class.

---
+++ source/DetectorWithAnalyticGaussianPSF.cpp
@@ -0,0 +1,491 @@
+#include "DetectorWithAnalyticPSF.h"
+
+/**
+ * \brief Constructor.
+ * 
+ * \details
+ * 
+ * The constructor initializes the groups in the HDF5 file where the different maps (i.e. pixel map,
+ * bias register map, smearing map, etc.) will be saved. 
+ * 
+ * The following maps are initialized to zero (partly through the base class Detector):
+ * 
+ * pixelMap 
+ * subPixelMap
+ * biasMap
+ * smearingMap
+ * flatfieldMap
+ * throughputMap
+ * cteMap
+ * 
+ * The flatfieldMap, throughputMap and cteMap are filled at pixel level.
+ * 
+ * \param configParam    Configuration parameters for the detector.
+ * \param hdf5file       HFD5 file to write the detector images to.
+ * \param camera         Camera to which to attach the detector.
+ */
+
+DetectorWithAnalyticGaussianPSF::DetectorWithAnalyticGaussianPSF(ConfigurationParameters &configParam, HDF5File &hdf5file, Camera &camera)
+: Detector(configParam, hdf5file, camera)
+{
+    // Parse the parameters from the configuration file.
+
+    configure(configParam);
+
+    // Create the groups in the HDF5 file where the different maps (i.e. pixel map,
+    // bias register map, smearing map, etc.) will be saved. This needs to be done
+    // BEFORE other methods write arrays to HDF5.
+
+    initHDF5Groups();
+
+    // Allocate memory for the flatfield map
+
+    flatfieldMap.ones(numRowsPixelMap, numColumnsPixelMap);
+
+    // Generate the flatfield map 
+
+    generateFlatfieldMap();
+
+}
+
+
+
+
+
+
+
+
+/**
+ * Destructor.
+ *
+ */
+DetectorWithAnalyticGaussianPSF::~DetectorWithAnalyticGaussianPSF()
+{
+    flushOutput();
+}
+
+
+
+
+
+
+
+
+
+
+/**
+ * \brief Configure the DetectorWithAnalyticGaussianPSF object using the ConfigurationParameters
+ * 
+ * \param configParam: the configuration parameters 
+ **/
+
+ void DetectorWithAnalyticGaussianPSF::configure(ConfigurationParameters &configParam)
+ {
+    // Treat the specific configurations for a analytical Gaussian PSF
+
+    sigmaX00 = configParam.getDouble("PSF/AnalyticGaussian/SigmaX00");
+    sigmaY00 = configParam.getDouble("PSF/AnalyticGaussian/SigmaY00");
+    sigmaX18 = configParam.getDouble("PSF/AnalyticGaussian/SigmaX18");
+    sigmaY18 = configParam.getDouble("PSF/AnalyticGaussian/SigmaY18");
+
+    // Get the configuration parameters for the PRNU
+
+    flatfieldNoiseAmplitude   = configParam.getDouble("CCD/FlatfieldPtPNoise");
+    includeFlatfield          = configParam.getBoolean("CCD/IncludeFlatfield");
+    flatfieldSeed             = configParam.getLong("RandomSeeds/FlatFieldSeed");
+ }
+
+
+
+
+
+
+
+
+
+
+ /**
+ * \brief: Generate the (random) flatfield variations.  This map is generated
+ *         at pixel level but without the edge pixels.
+ *
+ * https://github.com/python-acoustics/python-acoustics/blob/master/acoustics/generator.py#L108
+ */
+
+void DetectorWithAnalyticGaussianPSF::generateFlatfieldMap()
+{
+
+    Log.info("Detector: generating flatfield map.");
+
+    // Random number generation
+
+    mt19937 flatfieldGenerator(flatfieldSeed);
+    normal_distribution<double> flatfieldDistribution(0.0, 1.0);
+
+    // Double the dimensions (this is necessary because of the behaviour of the Fourier transforms)
+
+    int Nrows = 2 * numRowsPixelMap;
+    int Ncolumns = 2 * numColumnsPixelMap;
+
+    arma::cx_fmat evenMap = arma::cx_fmat(Nrows, Ncolumns);
+
+    for(unsigned int row = 0; row < Nrows; row++)
+    {
+        for(unsigned int column = 0; column < Ncolumns; column++)
+        {
+            // Fourier space: generate white noise and include 1/f dependency
+            // (Note: see https://en.wikipedia.org/wiki/Pink_noise#Generalization_to_more_than_one_dimension)
+
+            evenMap(row, column) = flatfieldDistribution(flatfieldGenerator) / (pow(row, 2) + std::pow(column, 2) + 1);
+        }
+    }
+
+    // Take the real part of the inverse Fourier transform
+
+    evenMap = arma::ifft2(evenMap);
+    arma::fmat realMap = arma::real(evenMap);
+
+    // Cut out the appropriate part
+
+    flatfieldMap(arma::span::all, arma::span::all) = realMap(arma::span(0, Nrows / 2 - 1), arma::span(0, Ncolumns / 2 - 1));
+
+    // Normalise
+
+    float minPinkNoise = flatfieldMap.min();
+    float maxPinkNoise = flatfieldMap.max();
+
+    flatfieldMap -= minPinkNoise;
+    flatfieldMap /= (maxPinkNoise - minPinkNoise); // [0, 1]
+    flatfieldMap *= flatfieldNoiseAmplitude;    // [0, flatfialdNoiseAmplitude]
+    flatfieldMap += (1.0 - flatfieldNoiseAmplitude);
+
+    // Write the result to the HDF5 output file
+
+    Log.debug("Detector: writing PRNU to HDF5");
+
+    hdf5File.writeArray("/Flatfield", "PRNU", flatfieldMap);
+}
+
+
+
+
+
+
+
+
+/**
+ * \brief: Zeroes the pixel, bias register, and the smearing maps.
+ *
+ * \pre pixel, bias register, and smearing maps filled with values from previous exposure.
+ *
+ * \post pixel, bias register, and smearing maps filled with zeroes.
+ */
+
+void DetectorWithAnalyticGaussianPSF::reset()
+{
+    pixelMap.zeros();
+    biasMap.zeros();
+    smearingMap.zeros();
+}
+
+
+
+
+
+
+
+
+
+
+/**
+ * \brief: Take an exposure with the detector starting at the given time.
+ *         The light is integrated during the given exposure time, during which 
+ *         the detector experiences the effects of jitter and thermo-elastic telescope 
+ *         drift. The background is assumed uniform for the whole subfield.
+ *         Afterwards, the collected light is read out, and various noise effects are added.
+ *
+ * \param startTime:    Starting time of the exposure [s].
+ * \param exposureTime: Duration of the exposure [s].
+ * 
+ * \return endTime:     Time after the exposure (startTime + exposureTime + readoutTime)
+ *
+ * \pre Pixel, bias register, and smearing map filled with values from previous exposure.
+ *
+ * \post Pixel unit in the pixel, bias register, and smearing maps: [ADU]
+ */
+
+double DetectorWithAnalyticGaussianPSF::takeExposure(double startTime, double exposureTime)
+{
+    // Advance the internal clock until the given start time
+
+    internalTime = startTime;
+
+    // Integration of point sources and background, taking into account jitter + drift.
+
+    Log.info("Detector: Integrating light for exposure " + to_string(imageNr) + " with exposure time = " + to_string(exposureTime));
+
+    integrateLight(startTime, exposureTime);
+
+    // Include noise effects like readout noise, photon noise, full well saturation, etc.
+    // Note: readOut() needs the exposure time to compute the open shutter smearing.
+
+    Log.info("Detector: Adding noise effects to exposure " + to_string(imageNr));
+
+    readOut(exposureTime);
+
+    // Write the CCD subfield, the bias map, and the smearing map to the HDF5 file
+
+    Log.debug("Detector: Writing PixelMap, smearing map, and bias map #" + to_string(imageNr) + " to HDF5 file.");
+
+    writePixelMapsToHDF5();
+
+    // Advance the internal clock
+
+    internalTime += exposureTime + readoutTime;
+
+    return internalTime;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * \brief: During an exposure, this method makes the detector integrate the light
+ *         in small steps. During each step the slight change of star positions due
+ *         to spacecraft jitter is taken into account. 
+ *         
+ *  \details  Besides jitter, also the sky background, and the flatfield is taken into 
+ *            account. The sub-pixel map is rebinned in a pixel map.  After rebinning,
+ *            vignetting and polarisation are applied (if applicable).
+ *
+ * \param startTime: Starting time of the exposure for which jitter must be applied [s].
+ *
+ * \pre Pixel, bias register, and smearing map filled with values from previous exposure.
+ *
+ * \post Pixel unit of the sub-pixel map: [photons].
+ * \post Pixel, bias register, and smearing map filled with zeroes.
+ */
+
+void DetectorWithAnalyticGaussianPSF::integrateLight(double startTime, double exposureTime)
+{
+
+    // Reset the subfield (i.e. get rid of the previous exposure, by zeroing the entire sub-field)
+
+    Log.debug("Detector: resetting subfield array for new exposure.");
+
+    reset();
+
+    // Integration (incl. jitter): point sources + background
+
+    camera.exposeDetector(*this, startTime, exposureTime);
+
+    // Apply flatfield (at pixel level)
+
+    if (includeFlatfield)
+    {
+        Log.debug("Detector: applying Flatfield.");
+
+        applyFlatfield();
+    }
+    else
+    {
+        Log.debug("Detector: no flatfield applied.");
+    }
+
+    // Apply throughput efficiency on the pixel map
+
+    applyThroughputEfficiency();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *  !!!!!!!!!!!!!  COMPLETLY REWRITE  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * \brief: Add the given flux value to the value of the sub-pixel that corresponds to the given coordinates 
+ *         in the focal plane. Return the pixel coordinates of the pixel to which the flux was added.
+ *
+ * \param xFP   X-coordinate of the (fractional) pixel in the focal plane in the FP reference frame [mm].
+ * \param yFP   Y-coordinate of the (fractional) pixel in the focal plane in the FP reference frame [mm].
+ * \param flux  Flux to add to the pixel map [photons].
+ *
+ * \return           (isInSubfield, row, col) 
+ *                   isInSubfield: True if (xFP, yFP) are on the subfield, false otherwise.
+ *                   row:          subfield (not CCD) row number of the pixel to which the flux was added
+ *                   col:          subfield (not CCD) column number of the pixel to which the flux was added  
+ */
+
+tuple<bool, double, double> DetectorWithAnalyticGaussianPSF::addFlux(double xFP, double yFP, double flux)
+{
+    // Convert the FP coordinates of the PSF barycenter to (real-valued) CCD pixel coordinates
+    // Then convert from CCD pixel coordinates to subfield pixel coordinates.
+
+    double row0, column0;
+    tie(row0, column0) = focalPlaneToPixelCoordinates(xFP, yFP);
+    row0 -= subFieldZeroPointRow;
+    column0 -= subFieldZeroPointColumn;
+
+    // Check if the star falls in the subfield. If not, don't add any flux, but simply return.
+
+    if (!isInPixelMap(row0, column0))
+    {
+        return make_tuple(false, row0, column0);
+    }
+
+    // Depending on the angular distance from the optical axis, the PSF increases in size. 
+    // Determine the standard deviations in both directions.
+
+    const double theta = rad2deg(camera.getGnomonicRadialDistanceFromOpticalAxis(xFP, yFP));
+    const double sigmaX = sigmaX00 + theta/18.0  * (sigmaX18 - sigmaX00);
+    const double sigmaY = sigmaY00 + theta/18.0  * (sigmaY18 - sigmaY00);
+
+    // Construct the covariance matrix of the rotated non-spherial gaussian
+
+    const double angle = atan2(yFP, xFP)
+    
+    arma::mat R(2,2);
+    R << cos(angle) << -sin(angle) << mat::endr
+      << sin(angle) <<  cos(angle) << mat::endr;
+    
+    arma::mat invS(2,2);
+    invS << 1./(sigmaX*sigmaX) <<         0          << arma::endr
+      <<            0          << 1./(sigmaY*sigmaY) << arma::endr;
+
+    arma::mat invCov = R * invS * R.t;                         // Inverse of the covariance matrix
+
+    const double detCov = 1./arma::det(invCov);                // Determination of the covariance matrix
+
+    //
+
+    arma::colvec mu = {row0, column0};
+
+    double sum = 0.0;
+
+    int range = max(10*floor(sigmaX), 10*floor(sigmaY));
+
+    for (int row = int(floor(row0))-range; row < int(floor(row0))+range+1; row++)
+    {
+        if ((row < 0) || (row >= numRowsPixelMap)) continue;
+
+        for (int col = int(floor(column0)))-range; col < int(floor(column0))+range+1; col++)
+        {
+            if ((col < 0) || (col >= numColumnsPixelMap)) continue;
+        
+            arma::colvec x = {row, col};
+
+            0.5 * (x - mu).t * invCov * (x-mu)
+
+            sum += 
+
+        }
+    }
+
+    // Normalize 
+
+
+    // That's it!
+    
+    return make_tuple(true, row, column);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * \brief   Check whether the given (row, column) indices are within the array range of the pixel map.
+ *
+ * \details  The input parameters row & column come from a coordinate transformation
+ *           in the focal plane, and as a result are not necessarily integers. For this 
+ *           function it's not necessary to round them to the nearest integer. 
+ *
+ * \param  row:    Row index. NOT a coordinate in the CCD frame, but in the subfield frame.    [pixel].
+ * \param  column: Column index. NOT a coordinate in the CCD frame, but in the subfield frame. [pixel].
+ *
+ * \return  True if the given (row, column) coordinates are in the pixel map; false otherwise.
+ */
+
+bool DetectorWithAnalyticGaussianPSF::isInPixelMap(double row, double column)
+{
+    return (column >= 0) && (row >= 0) && (column < numColumnsPixelMap) && (row < numRowsPixelMap);
+}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * \brief: Add the given flux value to (all sub-pixels of) the sub-pixel map.
+ *
+ * \param flux: Flux to add to the sub-pixel map [photons/pixel].
+ *
+ */
+
+void DetectorWithAnalyticGaussianPSF::addFlux(double flux)
+{
+    pixelMap += flux;
+}
+
+
+
+
+
+
+
+
+
+
+/**
+ * \brief: Multiply the sub-pixel map with the flatfield.
+ * 
+ * NOTE: The sub-pixel map contains extra edge pixels, but the flatfield
+ *       map does not. These edge pixels are excluded from this flatfield
+ *       multiplication.
+ *
+ * \pre Unit of the sub-pixels: [photons].
+ * \pre Flatfield map at sub-pixel level, excl. edge pixels.
+ * \pre Pixel, bias register, and smearing maps filled with zeroes.
+ *
+ * \post Pixel value in the sub-pixel map: [photons].
+ * \post Pixel, bias, and smearing maps filled with zeroes.
+ */
+
+void DetectorWithAnalyticGaussianPSF::applyFlatfield()
+{
+    const unsigned int beginRow = numEdgePixels;
+    const unsigned int beginCol = numEdgePixels;
+    const unsigned int endRow = numRowsPixelMap - numEdgePixels - 1;
+    const unsigned int endCol = numColumnsPixelMap - numEdgePixels - 1;
+
+    pixelMap.submat(beginRow, beginCol, endRow, endCol) = pixelMap.submat(beginRow, beginCol, endRow, endCol) % flatfieldMap;
+}
+
