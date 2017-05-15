04f4718a4911f6e66ae0b0ff8cff4f508405ae03
Rik Huygen <rik.huygen@ster.kuleuven.be>
2017-05-15 09:40:22 +0200

    Updated HTML and Search files for documentation.

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['mainpage_2emd',['MainPage.md',['../_main_page_8md.html',1,'']]],
-  ['mainpage',['MainPage',['../md_docs_source_MainPage.html',1,'']]]
+  ['mainpage_2emd',['MainPage.md',['../_main_page_8md.html',1,'']]]
 ];
