03c11c77626a3a16ab9f4220e21a8394c50e634e
Joris De Ridder <joris@ster.kuleuven.be>
2017-09-21 17:16:29 +0200

    Added a unit conversion factor to the constructor of Parameter. This way we can express a quantity in SI in a time series file, but use a more convenient unit inside the simulation code.

--- include/Parameter.h
+++ include/Parameter.h
@@ -21,8 +21,8 @@ class Parameter
 {
     public:
         
-        Parameter(T currentValue);
-        Parameter(string filePath);
+        Parameter(T fixedValue);
+        Parameter(string filePath, T unitConversionFactor);
         ~Parameter();
 
         const T operator()(const double time);
@@ -37,9 +37,10 @@ class Parameter
     private:
 
         bool isFixedToValue;         // True if the parameter is constant in time, false otherwise.
-        T currentValue;              // The 
+        T currentValue;              // The parameter value for the last updated time point 
         string filePath;             // Name of time series file, if the parameter is not constant in time
         ifstream inputFile;          // Time series file
+        T unitConversionFactor;      // Unit conversion factor to multiply the values in the file with.
 
         double time1;                // Only two consecutive pairs (time1, value1) and (time2, value2)
         double time2;                //      are read and stored (rather than reading and storing the entire
@@ -84,8 +85,8 @@ Parameter<T>::Parameter(T fixedValue)
  */
 
 template <typename T>
-Parameter<T>::Parameter(string filePath)
-    : isFixedToValue(false), filePath(filePath)
+Parameter<T>::Parameter(string filePath, T unitConversionFactor)
+    : isFixedToValue(false), filePath(filePath), unitConversionFactor(unitConversionFactor)
 {
    openFile(filePath);
 
@@ -171,6 +172,7 @@ void Parameter<T>::updateValue(double time)
     const double weight1 = (time - time1) / (time2 - time1); 
     const double weight2 = (time2 - time) / (time2 - time1); 
     currentValue = value1 * weight2 + value2 * weight1;
+
 }
 
 
@@ -269,6 +271,11 @@ pair<double, T> Parameter<T>::readNextFromFile()
     stringstream reader(buffer);
     reader >> timeAndValue.first;
     reader >> timeAndValue.second;
+
+    // The values in the file might be SI units, while the code may need a different unit.
+    // Hence we allow the user the specify a unit conversion factor. 
+
+    timeAndValue.second *= unitConversionFactor;       
 
     // That's it!
 
