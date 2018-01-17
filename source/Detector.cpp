bd59c96146df6dcd9022799b8daa26bb46ace7a8
Sara Regibo <sara@ster.kuleuven.be>
2018-01-17 15:42:41 +0100

    Implementation of dark signal.
    Drawbacks of the current implementation:
    * Only the pixel map is affected.
    * The contribution of the dark signal to the image area is added in one go for the exposure and readout together.  It may be better to add the contribution of the dark signal during exposure right before the noise effects are added and add the contribution of the dark current during the readout further down the chain.  To be investigated.

--- source/Detector.cpp
+++ source/Detector.cpp
@@ -26,8 +26,9 @@
  */
 
 Detector::Detector(ConfigurationParameters &configParam, HDF5File &hdf5file, Camera &camera, TemperatureGenerator &feeTemperatureGenerator, TemperatureGenerator &detectorTemperatureGenerator)
-: HDF5Writer(hdf5file), 
+: HDF5Writer(hdf5file),
   includeCosmics(true),
+  includeDarkSignal(true),
   includePhotonNoise(true),
   includeReadoutNoise(true),
   includeCTIeffects(true), 
@@ -68,6 +69,9 @@ Detector::Detector(ConfigurationParameters &configParam, HDF5File &hdf5file, Cam
 
     // Set the seeds of the random number generators
 
+    darkSignalGenerator.seed(darkSignalSeed);
+    darkNoiseGenerator.seed(darkSignalSeed + 1);
+
     photonNoiseGenerator.seed(photonNoiseSeed);
     readoutNoiseGenerator.seed(readoutNoiseSeed);
 
@@ -83,6 +87,7 @@ Detector::Detector(ConfigurationParameters &configParam, HDF5File &hdf5file, Cam
 
     double exposureTime = configParam.getDouble("ObservingParameters/ExposureTime");
 
+    fastForwardDarkSignalGeneratorToExposure(beginExposureNr, exposureTime);
     fastForwardReadoutNoiseGeneratorToExposure(beginExposureNr);
     fastForwardPhotonNoiseGeneratorToExposure(beginExposureNr);
     fastForwardCosmicsGeneratorToExposure(beginExposureNr, exposureTime);
@@ -195,6 +200,8 @@ void Detector::updateParameters(double time)
     cosmicHitRate                       = configParam.getDouble("Sky/Cosmics/CosmicHitRate");
     cosmicTrailLength                   = configParam.getDoubleVector("Sky/Cosmics/TrailLength");
     cosmicIntensity                     = configParam.getDoubleVector("Sky/Cosmics/Intensity");
+    darkCurrent                         = configParam.getDouble("CCD/DarkSignal/DarkCurrent");
+    dsnu                                = configParam.getDouble("CCD/DarkSignal/DSNU");
     fullWellSaturationLimit             = configParam.getLong("CCD/FullWellSaturation");
     digitalSaturationLimit              = configParam.getLong("CCD/DigitalSaturation");
     readoutNoise                        = configParam.getDouble("CCD/ReadoutNoise");
@@ -515,6 +522,44 @@ void Detector::applyThroughputEfficiency()
     
     pixelMap = pixelMap % throughputMap;
     
+}
+
+
+
+
+
+/**
+ *\brief Adds the dark signal to the pixel map.  This follows a normal distribution,
+ *       centered around the dark current and with the DSNU (percentage of the dark signal)
+ *       as standard deviation.  The noise of the simulated dark signal is of the order
+ *       of the sqrt of the dark signal (simulated with a normal distribution).
+ *
+ * \param exposureTime: Exposure time [s].
+ *
+ * \pre Pixel unit in the pixel map: [electrons].
+ * \pre Bias register and smearing maps filled with zeroes.
+ */
+void Detector::addDarkSignal(float exposureTime)
+{
+	double darkSignalRef = darkCurrent * (exposureTime + readoutTime);
+	darkSignalDistribution = normal_distribution<double>(darkSignalRef, darkSignalRef * dsnu / 100.0);
+
+	double darkSignal;
+
+	for(unsigned int row = 0; row < numRowsPixelMap; row++)
+	{
+		for(unsigned int column = 0; column < numColumnsPixelMap; column++)
+		{
+
+			darkSignal = darkSignalDistribution(darkSignalGenerator);		// Take DSNU into account
+
+			darkNoiseDistribution = normal_distribution<double>(darkSignal, sqrt(darkSignal));
+			darkSignal = darkNoiseDistribution(darkNoiseGenerator);
+
+
+			pixelMap(row, column) += darkSignal;
+		}
+	}
 }
 
 
@@ -1938,6 +1983,47 @@ void Detector::writePixelMapsToHDF5(int exposureNr)
     // Add the bias map to the "BiasMaps" group
 
     hdf5File.writeArray("/ThroughputMaps", throughputMapName, throughputMap);
+}
+
+
+
+
+
+
+
+/**
+ * \brief PlatoSim allows to generate e.g. exposures 0->99, and then a second run
+ *        from 100->199, which facilitates Slurming long time series. The results
+ *        should be the same as if we would generate 0->199 in one stretch. Therefore,
+ *        when generating exposures 100->199, we have to take care that all noise
+ *        generators start in the correct state. The way to do this, is to fast-forward
+ *        through all noise generations of exposures 0->99. This function is therefore
+ *        a skeleton version of addDarkSignal(), where noise is generated but further
+ *        ignored.
+ */
+void Detector::fastForwardDarkSignalGeneratorToExposure(int beginExposureNr, float exposureTime)
+{
+	double dark = darkCurrent * (exposureTime + readoutTime);
+
+	darkSignalDistribution = normal_distribution<double>(dark, dark * dsnu / 100.0);
+
+	double dummy;
+
+	for (int n = 0; n < beginExposureNr; n++)
+	{
+	    // The readout noise generated for the pixel map
+
+	    for (unsigned int row = 0; row < numRowsPixelMap; row++)
+	    {
+	         for (unsigned int column = 0; column < numColumnsPixelMap; column++)
+	         {
+	        	 	 dummy = darkSignalDistribution(darkSignalGenerator);	// Take DSNU into account
+
+	        	 	 darkNoiseDistribution = normal_distribution<double>(dummy, sqrt(dummy));
+	        	 	 dummy = darkNoiseDistribution(darkNoiseGenerator);
+	         }
+	    }
+	}
 }
 
 
