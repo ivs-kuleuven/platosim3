d9e3ea28964d270a554d793ba5ccc8a3ca7ad871
Joris De Ridder <joris@ster.kuleuven.be>
2018-08-28 16:50:56 +0200

    Changed Log.warning() to Log.info() in ConfigurationParameters::setParameter().

--- source/ConfigurationParameters.cpp
+++ source/ConfigurationParameters.cpp
@@ -525,7 +525,7 @@ void ConfigurationParameters::setParameter(const string &key, const string &valu
         {
             YAML::Node subnode = node[fields[1]];
             if (subnode)
-                Log.warning("ConfigurationParameters: setParameter() overwrites subnode \"" + fields[1] + "\" of node \"" + fields[0]);
+                Log.info("ConfigurationParameters: setParameter() overwrites subnode \"" + fields[1] + "\" of node \"" + fields[0]);
 
             node[fields[1]] = value;
         }
@@ -537,11 +537,14 @@ void ConfigurationParameters::setParameter(const string &key, const string &valu
     else
     {
         if (config[key])
-            Log.warning("ConfigurationParameters: setParameter() overwrites node \"" + key);
+            Log.info("ConfigurationParameters: setParameter() overwrites node \"" + key);
             
         config[key] = value;
     }
 }
+
+
+
 
 
 
