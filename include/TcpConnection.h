46c209036fcbe6a4490aeb3990b45533f0e22d53
BertoltLang <lang.bertolt@gmx.de>
2019-04-09 17:07:31 +0200

    wrote the connectToServer() function which requests a jitter step from server and sends it to the jitterFromNetwork object

--- include/TcpConnection.h
+++ include/TcpConnection.h
@@ -11,7 +11,7 @@
 class TcpConnection
 {
 	public:
-		TcpConnection(ConfigurationParameters &configParam, JitterGenerator* jitterFromNetwork);
+		TcpConnection(ConfigurationParameters &configParam, JitterGenerator* jitterFromNetwork, std::condition_variable* cond_var, std::mutex* m, bool* notified, bool* newStep);
 		~TcpConnection();
 
 		void connectToServer();
@@ -22,8 +22,17 @@ class TcpConnection
 	private:
 
 		void configure(ConfigurationParameters &configParams);
+		std::vector <double> processServerReply(string replyString);
 
 		bool endOfSimulation;
+		string tcpAddress;
 
 		JitterGenerator* jitterInstance;
+
+		double internalTime;
+
+		std::condition_variable* condVarPointer;
+		std::mutex* mutexPointer;
+		bool* notifiedPointer;
+		bool* newStepPointer;
 };
--- source/TcpConnection.cpp
+++ source/TcpConnection.cpp
@@ -6,14 +6,18 @@
  *		  
  */
 
-TcpConnection::TcpConnection(ConfigurationParameters &configParam, JitterGenerator* jitterFromNetwork)
+TcpConnection::TcpConnection(ConfigurationParameters &configParam, JitterGenerator* jitterFromNetwork, std::condition_variable* cond_var, std::mutex* m, bool* notified, bool* newStep)
 {
-	// declare a pointer to the jitterFromNetwork innstance
+	// set some starting parameters
 
 	jitterInstance = jitterFromNetwork;
 
-	endOfSimulation = false;
+	configure(configParam);
 
+	condVarPointer = cond_var;
+	mutexPointer = m;
+	notifiedPointer = notified;
+	newStepPointer = newStep;
 }
 
 
@@ -25,7 +29,11 @@ TcpConnection::TcpConnection(ConfigurationParameters &configParam, JitterGenerat
 
 void TcpConnection::configure(ConfigurationParameters &configParams)
 {
+	endOfSimulation = false;
 
+	internalTime = -1.0;
+
+	tcpAddress = configParams.getString("Platform/TcpAddress");
 }
 
 /**
@@ -46,8 +54,14 @@ TcpConnection::~TcpConnection()
  */
 void TcpConnection::connectToServer()
 {
+	// declare socket
+
+	zmq::context_t context(1);
+	zmq::socket_t socket(context, ZMQ_REQ);
+
 	// connect to the server
 
+	socket.connect(tcpAddress);
 	
 	// repeat until the simulation is over
 
@@ -56,31 +70,95 @@ void TcpConnection::connectToServer()
 
 		// declare a lock
 
+		 std::unique_lock<std::mutex> lock(*mutexPointer);
 
 		// wait for a notification from the simulation
 
+		while(!*notifiedPointer)
+            	{
+                	condVarPointer->wait(lock);
+            	}
 
-		// send a request to the server for the next jitter step
+                // if the thread is notified, a request to the server should be send
 
-		
-		// get the reply from server
+               	// reset to the initial parameters
+               	*notifiedPointer = false;
+               	lock.unlock();
+		bool validStep = false;
 
-		
-		// process the jitter data
+		// request new jitter steps until a valid step is send
+		while(!validStep)
+		{
+			// send a request to the server for the next jitter step
+	
+	                zmq::message_t request (5);
+	                memcpy(request.data(), "New Jitter step, please.", 5);
+			socket.send(request);
+			
+			// get the reply from server
+			zmq::message_t reply;
+	                socket.recv(&reply);
+			
+			// process the jitter data
 
+			std::string replyString = std::string(static_cast<char*>(reply.data()), reply.size());
+	
+			std::vector<double> currentJitterStepVec = processServerReply(replyString);
+	
 
-		// set the jitter step in the jitter generator object
+			// set the jitter step in the jitter generator object if its time stamp is higher than the internal time
+	
+	            	if (currentJitterStepVec.at(1) > internalTime)
+	            	{
+	                	jitterInstance->setCurrentJitterStep(currentJitterStepVec.at(0), currentJitterStepVec.at(1), currentJitterStepVec.at(2), currentJitterStepVec.at(3), currentJitterStepVec.at(4));
+	
+	                	*newStepPointer = true;
+	
+	                	condVarPointer->notify_one();
 
-
-		// unlock the simulation thread
-
-		
-		// check whether the simulation is at its end
-
-
+				validStep = true;     
+	            	}
+			
+			// check whether the simulation is at its end
+	
+			if (currentJitterStepVec.at(0) != 0)
+	           	{
+	                	endOfSimulation = true;
+	            	}	
+	            	else
+	            	{
+	                	endOfSimulation = false;
+            		}
+		}
 	}
+}
+
+std::vector <double> TcpConnection::processServerReply(string replyString)
+{
+	// fracture the received string
+
+        std::stringstream ss(replyString);
+
+	std::vector<double> currentJitterStepVec;
+
+        double i;
+
+	// convert the string to double values
+
+        while (ss >> i)
+        {
+             currentJitterStepVec.push_back(i);
+
+             if (ss.peek() == ',' || ss.peek() == ' ')
+             {
+                 ss.ignore();
+             }
+         }
+	
+	return currentJitterStepVec;
 
 }
+
 
 
 
