d51eac0f11cc645cc3244ca1d76230c406a186ee
BertoltLang <lang.bertolt@gmx.de>
2019-05-07 18:05:31 +0200

    altered the testServer, that it will now read from a jitter file and send its steps instead of random values

--- testServer.cpp
+++ testServer.cpp
@@ -1,76 +1,110 @@
 //
 //  Binds REP socket to tcp://*:5555
 //
+
+#include <chrono>
+#include <thread>
 #include <sstream>
+#include <fstream>
 #include <zmq.hpp>
 #include <time.h>
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <chrono>
+
 #ifndef _WIN32
 #include <unistd.h>
 #else
 #include <windows.h>
 
-
-#define sleep(n)    Sleep(n)
 #endif
+
+
+std::string jitterFileName = std::getenv("PLATO_PROJECT_HOME") + std::string("/inputfiles/ohb18jun.txt");
+int numberOfSteps = 80;
 
 int main () 
 {
-	//prepare random seed
-	srand(time(NULL));
-
     //  Prepare our context and socket
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REP);
     socket.bind ("tcp://*:5555");
 
+    //create an ifstream
+    std::ifstream infile(jitterFileName);
+
+    double step, yaw, pitch, roll;
+    int stepCounter = 0;
+
+    std::vector<double> stepVec;
+    std::vector<double> yawVec;
+    std::vector<double> pitchVec;
+    std::vector<double> rollVec;
+
     int endOfSimulation = 0;
 
-    double jitterStep = 0.0;
-    double yaw;
-    double pitch;
-    double roll;
-
-    int counter = 0;
-
-    while (!endOfSimulation) 
+    // read the file line by line and save the values in the respective vector
+    while (infile >> step >> yaw >> pitch >> roll)
     {
-        if (counter == 100)
+        // make sure not to read more lines than used in the 
+        if (stepCounter >= numberOfSteps)
         {
-            endOfSimulation = 1;
+            break;
         }
+        else
+        {
+            stepVec.push_back(step);
+            yawVec.push_back(yaw);
+            pitchVec.push_back(pitch);
+            rollVec.push_back(roll);
+            stepCounter++;
+        }
+    }
+
+    // get the time between steps (assuming it won't change) in milliseconds
+	
+    // default, if there is only one entry in the file
+    int stepLength = 1;
+
+    if (stepVec.size() > 1)
+    {
+        stepLength = (stepVec.at(1) - stepVec.at(0)) * 1000;
+    }
+
+    std::vector<double> timeVec;
+    double completeTime = 0;
+
+    for (int i = 0; i < numberOfSteps; i++)
+    {
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
         zmq::message_t request;
 
         //  Wait for next request from client
         socket.recv (&request);
-        std::cout << "Received Hello" << std::endl;
+        std::cout << "Received request for next jitter step" << std::endl;
 
-        //  Do some 'work'
-        sleep(1);
+        // Do some 'work'
+        std::this_thread::sleep_for(std::chrono::milliseconds(stepLength));
 
-	// create some random jitter values
+	if (i == stepVec.size()-1)
+        {
+            std::cout << "end of simulation" << std::endl; 
+            endOfSimulation = 1;
+        }
 
-	yaw = (rand() % 100) / 10000.0;
-	pitch = (rand() % 100) / 10000.0;
-	roll = (rand() % 100) / 10000.0;
-
-
-        std::ostringstream strEos; //, strJs, strY, strP, strR;
+        std::ostringstream strEos;
         std::ostringstream strJs;
         std::ostringstream strY;
         std::ostringstream strP;
         std::ostringstream strR;
 
-        jitterStep += 0.1;
-
         strEos << endOfSimulation;
-        strJs << jitterStep;
-        strY << yaw;
-        strP << pitch;
-        strR << roll;
+        strJs << stepVec.at(i);
+        strY << yawVec.at(i);
+        strP << pitchVec.at(i);
+        strR << rollVec.at(i);
 
         std::string message = strEos.str() + "," + strJs.str() + "," + strY.str() + "," + strP.str() + "," + strR.str();
 
@@ -86,8 +120,21 @@ int main ()
         memcpy (reply.data (), cMessage, strLength);
         socket.send (reply);
 
-        counter++;
+	std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+        
+        double timeDiff = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() /1000000.0;
+
+	timeVec.push_back(timeDiff);
+
+        completeTime += timeDiff;
+
     }
+
+    
+
+    std::cout << *max_element(std::begin(timeVec), std::end(timeVec)) << std::endl;
+
+    std::cout << completeTime/timeVec.size() << std::endl;
 
     return 0;
 }
