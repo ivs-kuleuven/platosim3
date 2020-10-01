6ffa07a56aaa4f043667da9896756d355ab874ce
Sara Regibo <sara@ster.kuleuven.be>
2020-10-01 10:08:03 +0200

    Update the user documentation.

--- navtreedata.js
+++ navtreedata.js
@@ -2,10 +2,11 @@ var NAVTREE =
 [
   [ "PLATO Simulator", "index.html", [
     [ "Installing/Updating PlatoSim3", "InstallUsersDevelopers.html", [
-      [ "For Users", "ViaConda.html", [
-        [ "Prerequisites", "ReqsInstallViaConda.html", null ],
-        [ "Installing (and Updating)", "InstallViaConda.html", null ],
-        [ "A Word about Jenkins", "AboutJenkins.html", null ]
+      [ "For Users", "user-overview.html", [
+        [ "Prerequisites", "user-prerequisites.html", null ],
+        [ "Installing (and Updating)", "user-install.html", null ],
+        [ "A Word about Jenkins", "user-jenkins.html", null ],
+        [ "Wrap-Up", "user-wrap-up.html", null ]
       ] ],
       [ "For Developers", "dev-overview.html", [
         [ "Prerequisites", "dev-prerequisites.html", null ],
@@ -17,22 +18,19 @@ var NAVTREE =
         [ "Contributing to the Code", "dev-push.html", null ],
         [ "Branching Strategy", "dev-branching.html", null ],
         [ "Wrap-Up", "dev-wrap-up.html", null ]
+      ] ]
+    ] ],
+    [ "Running PlatoSim3", "Running.html", [
+      [ "Input Files", "InputFileDescription.html", [
+        [ "Configuration Parameters", "ConfigurationParameters.html", null ],
+        [ "Supplementary Files", "SupplementaryFiles.html", null ],
+        [ "Migration Tool", "MigrationTool.html", null ]
       ] ],
-      [ "Running PlatoSim3", "Running.html", [
-        [ "Prerequisites", "ReqsRun.html", null ],
-        [ "Input files", "InputFileDescription.html", [
-          [ "Configuration Parameters", "ConfigurationParameters.html", null ],
-          [ "Supplementary Files", "SupplementaryFiles.html", null ],
-          [ "Migration Tool", "MigrationTool.html", null ]
-        ] ],
-        [ "Running Simulations", "Simulating.html", [
-          [ "Scripting in Python", "ScriptingInPython.html", null ],
-          [ "Tutorials", "@ref tutorials", null ]
-        ] ],
-        [ "Simulation Steps", "SimulationSteps.html", null ],
-        [ "Output files", "OutputFileDescription.html", null ]
+      [ "Running Simulations", "Simulating.html", [
+        [ "Scripting in Python", "ScriptingInPython.html", null ],
+        [ "Tutorials", "@ref tutorials", null ]
       ] ],
-      [ "Help (Issue Tracking)", "IssueTracking.html", null ]
+      [ "Simulation Steps", "SimulationSteps.html", null ]
     ] ]
   ] ]
 ];
--- search/files_3.js
+++ search/files_3.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['inputfile_2emd',['InputFile.md',['../_input_file_8md.html',1,'']]],
   ['install4usersanddevelopers_2emd',['Install4UsersAndDevelopers.md',['../_install4_users_and_developers_8md.html',1,'']]],
-  ['installingviaconda_2emd',['InstallingViaConda.md',['../_installing_via_conda_8md.html',1,'']]],
   ['issuetracking_2emd',['IssueTracking.md',['../_issue_tracking_8md.html',1,'']]]
 ];
