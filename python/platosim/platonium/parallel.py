687199fcfac3c1b13f7f822f26b9dba09ef11965
Nicholas Jannsen <nicholasemborgjannsen@gmail.com>
2023-08-25 13:40:33 +0200

    Update test of script parallel

--- python/platosim/platonium/parallel.py
+++ python/platosim/platonium/parallel.py
@@ -32,8 +32,10 @@ C = params[:,2]
 Q = params[:,3]
 N = len(S)
 
-def run_platonium(S,G,C,Q):
-    os.system(f'platonium {S} {G} {C} {Q} --project {project} --nexp 1 -w')
+def run_platonium(i, S,G,C,Q):
+    print(f'Simulation {i}', end='\r')
+    PLATOnium = os.getenv('PLATO_PROJECT_HOME') + '/python/platosim/platonium/platonium'
+    os.system(f'{PLATOnium} {S} {G} {C} {Q} --project {project} --nexp 1 -w -v 0')
 
 with parallel_config(backend=backend, n_jobs=cpus):
-    Parallel()(delayed(run_platonium)(S[i],G[i],C[i],Q[i]) for i in range(N))
+    Parallel()(delayed(run_platonium)(i,S[i],G[i],C[i],Q[i]) for i in range(N))
