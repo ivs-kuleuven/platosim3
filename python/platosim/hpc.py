b0cb910bfb06eff8e204b5beb51f71688b0cbde5
Nicholas Jannsen <nicholasemborgjannsen@gmail.com>
2023-10-08 21:13:50 +0200

    More general call to hpc class for parallisation

--- python/platosim/hpc.py
+++ python/platosim/hpc.py
@@ -18,9 +18,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# PlatoSim imports
+from platosim.utilities import errorcode
+
 # Disable warnings
 #warnings.simplefilter("ignore")
-
 
 
 #==============================================================#
@@ -44,7 +46,7 @@ class HPC(object):
         self.path = Path(__file__).parent.resolve()
         self.platoInputDir   = self.path.joinpath(os.getenv('PLATO_PROJECT_HOME'), 'inputfiles')
 
-
+        
 
 
 
@@ -62,13 +64,9 @@ class HPC(object):
         with parallel_config(backend=self.backend, n_jobs=self.cpus):
 
             if script == 'platonium':
-                S = params[:,0]
-                G = params[:,1]
-                C = params[:,2]
-                Q = params[:,3]
-                N = len(S)
+                N, M = params.shape
                 Parallel()(delayed(self.run_platonium)
-                           (i, N, S[i], G[i], C[i], Q[i], project)
+                           (i, N, M, params, project)
                            for i in range(N))
 
             if script == 'varsim':
@@ -80,29 +78,52 @@ class HPC(object):
                            for i in range(N))
 
             # Print when script is done
-            errorcode('message', 'Parallel simulations are done!')
+            errorcode('message', 'All simulations finished successfully!')
 
 
-                
-                
+
             
-    def run_platonium(self, i, N, S, G, C, Q, project):
+    #def set_compress(self, compress=False): return compress
+    #def set_nexp(self,    nexp    = ''):  self.nexp     = nexp
+    def set_seed(self,    seed    = ''):  self.seed     = seed
+    def set_cadence(self, cadence = ''):  self.cadence  = cadence
+    def set_vardir(self,  vardir  = ''):  self.vardir   = vardir
 
-        """Function to run the parallelisation.
+        
+    def run_platonium(self, i, N, M, params, project):
+
+        """Function to run the PLATOnium in parallel.
         """
         
         print(f'Simulation {i+1}/{N} -> ({round(i/N*100,2)}%)', end='\r')
-        PLATOnium = os.getenv('PLATO_PROJECT_HOME') + '/python/platosim/platonium/platonium.py'
+        platonium = os.getenv('PLATO_PROJECT_HOME') + '/python/platosim/platonium/platonium.py'
+        
+        S = int(params[i,0])
+        G = int(params[i,1])
+        C = int(params[i,2])
+        Q = int(params[i,3])
 
-        #if mode == 'normal':
-        #    os.system(f'{PLATOnium} {S} {G} {C} {Q} --project {project} --nexp 1 -w -v 0')
-        #elif mode == 'statistics':
-        os.system(f'{PLATOnium} {S} {G} {C} {Q} --project {project} --statistics')
+        # Nine digit star ID 
+        starID = f'{S}'.zfill(9)
+        odir   = os.getenv('PLATO_WORKDIR') + f'/{project}/output/{starID}'
+        # Parse flags
+        #compress = self.set_compress()
+
+        # Parse arguments
+        #if self.set_nexp    != '': nexp     = f'--nexp {self.nexp}'
+        if self.set_seed    != '': seed     = f'--seed {self.seed}'
+        if self.set_cadence != '': cadence  = f'--cadence {self.cadence}'
+        if self.set_vardir  != '': varfile  = f'--varfile {self.vardir}/varsource_{starID}.txt'
+
+        # Run PlatoSim simulation
+        os.system(f'{platonium} {S} {G} {C} {Q} --project {project} -o {odir} ' +
+                  f'{seed} {cadence} {varfile} ' +
+                  '--compress -v 0 -w')
 
 
 
 
-
+        
     def run_varsim(self, i, N, S, odir):
 
         """Function to run the parallelisation.
